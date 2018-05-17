c58ebe27519ee1af009c8f6ed998a53bdd78ca38
Kevin <stradtkt22@gmail.com>
2018-05-17 15:25:43 -0400

    weather-api added

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+    var apiKey = 'b1d06f71e0d2f85661337cfefe5c2578';
+    var city = $('input#city').val();
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid='+apiKey;
+    $.get(url, function(res) {
+      var html = '';
+      html += '<p class="lead">'+res.name+'</p>';
+      html += '<p>'+res.weather[0].description+'</p>';
+      html += '<p>'+res.main.temp+'</p>'
+      $('.weather').html(html);
+    }, 'jsonp');
+    return false;
+  });
+});
